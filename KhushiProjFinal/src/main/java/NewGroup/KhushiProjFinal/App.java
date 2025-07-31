b3c9814fc2eb8bf3898760a4bb2a8b82e8e9fc86
Khushi Shah <khushishah3502@gmail.com>
2025-07-31 08:59:11 +0530

    Hey, i added new repo named Khuhshifinaltesting

---
+++ KhushiProjFinal/src/main/java/NewGroup/KhushiProjFinal/App.java
@@ -0,0 +1,13 @@
+package NewGroup.KhushiProjFinal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ KhushiProjFinal/src/test/java/NewGroup/KhushiProjFinal/Test1.java
@@ -0,0 +1,13 @@
+package NewGroup.KhushiProjFinal;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void t1()
+	{
+		System.out.println("This is test1 method");
+	}
+
+}
